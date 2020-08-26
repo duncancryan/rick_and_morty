d4e86a62da9a93423a960867b68f95575b25f819
Duncan Ryan <duncancryan1@gmail.com>
2020-08-26 23:02:05 +0100

    pulled data I wanted in, also added selectedCharacter to my data for later on

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  
+  <div>
+    <h1>Characters</h1>
+    <div class="main-container">
+      <character-list>
+
+      </character-list>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,14 +14,17 @@ export default {
   name: 'app',
   data(){
     return {
-
+      characters: [],
+      selectedCharacter: null
     };
   },
   mounted(){
-
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(characters => this.characters = characters.results)
   },
   components: {
-    
+
   }
 
 }
